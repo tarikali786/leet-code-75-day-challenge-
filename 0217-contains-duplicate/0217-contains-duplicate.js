c262f225ee0fef9f3b00f9f7be234d62bb2af24b
Tarik Ali <110152841+tarikali786@users.noreply.github.com>
2026-03-13 10:50:42 +0530

    Time: 16 ms (73.43%), Space: 72.5 MB (90.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,16 +4,8 @@
  */
  const  arrayValue = [1,1,1,3,3,4,3,2,4,2]
 var containsDuplicate = function(nums) {
-    let seen = new Set()
-
-    for (let i = 0; i < nums.length; i++) {
-        if (seen.has(nums[i])) {
-            return true
-        }
-        seen.add(nums[i])
-    }
-
-    return false
+    const unique = new Set(nums);
+    return unique.size !== nums.length;
 };
 
 const result = containsDuplicate(arrayValue)
