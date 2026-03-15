f4ef3fcf14e47dddeafc03683c5fe0269e9fe0b9
Tarik Ali <110152841+tarikali786@users.noreply.github.com>
2026-03-15 19:41:07 +0530

    Time: 4 ms (98.65%), Space: 72.8 MB (64.58%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+      for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1
+        
+        if (nums[index] > 0) {
+            nums[index] = -nums[index]
+        }
+    }
+
+    let result = []
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            result.push(i + 1)
+        }
+    }
+
+    return result
+};
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
